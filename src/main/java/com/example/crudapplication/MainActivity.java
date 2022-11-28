fb1c5810fdff0c9828572f0f40fbbd9670467201
Hank <hunterhope@outlook.com>
2022-11-28 15:01:10 +0800

    重來一次 嘗試 TDD撰寫方式

--- src/main/java/com/example/crudapplication/MainActivity.java
+++ src/main/java/com/example/crudapplication/MainActivity.java
@@ -2,11 +2,9 @@ package com.example.crudapplication;
 
 import android.os.Bundle;
 
-import com.example.crudapplication.ui.result.ResultViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,13 +12,10 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.crudapplication.databinding.ActivityMainBinding;
 
-import dagger.hilt.android.AndroidEntryPoint;
-
-@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private AppBarConfiguration appBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,27 +24,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-
-        appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_result)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-        ResultViewModel resultViewModel=new ViewModelProvider(this).get(ResultViewModel.class);
-        navView.findViewById(R.id.navigation_create).setOnClickListener(v->{
-            resultViewModel.createEmployee();
-            navController.navigate(R.id.navigation_create);
-        });
-
-    }
-
-    //up按鈕動作
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 
 }
